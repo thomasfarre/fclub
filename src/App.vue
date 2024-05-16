7412c49505831e5b29884a46aee8f2a1ec992bb9
Thomas FARRE <tfarre@sutunam.com>
2024-05-16 22:52:22 +0200

    first elements homepage

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@
 </script>
 
 <template>
-  <div id="app">
-    <header>
-      <nav>
+  <div id="app" class="max-w-4xl mx-auto py-6">
+    <header class="flex space-x-12 items-center">
+      <div >
+        <img class="w-28 h-14" src="https://v-images.cdnsw.com/Root/durlh/logoflcub2.png?s=YqFAknhl" alt="">
+      </div>
+      <nav class="flex items-center space-x-6">
+        <a>Le club</a>
+        <a>Activités</a>
+        <a>Activités Enfants</a>
+        <a>Actu/Vie du club</a>
         <router-link to="/login">Login</router-link>
         <router-link to="/signup">Signup</router-link>
       </nav>
@@ -13,6 +20,9 @@
     <main>
       <router-view></router-view>
     </main>
+    <!-- <footer>
+      footer
+    </footer> -->
   </div>
 </template>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,18 @@
 <template>
-  <div class="min-h-screen flex flex-col">
+  <div class="pt-12">
     <!-- Hero Section -->
-    <section class="bg-blue-600 text-white text-center p-20">
-      <h1 class="text-4xl font-bold">Welcome to Our Boxing Club</h1>
-      <p class="mt-4 text-lg">Join us to learn, train, and compete in boxing.</p>
-      <button class="mt-8 bg-white text-blue-600 py-2 px-4 rounded-full font-semibold">Join Now</button>
+    <section class="relative h-[580px] w-full rounded-md overflow-hidden">
+      <img class="inset-0 absolute object-cover w-full h-full" src="/bg-1.jpg" alt="">
+      <div class="absolute z-10 left-0 top-1/2 bg-white opacity-90">
+        <div class="pt-12 pl-6 pr-60">
+          <h1 class="text-4xl font-bold text-green-700">F-CLUB</h1>
+          <span>Salle de sports de combat et de Pilates</span>
+        </div>
+        <div class="transform translate-y-1/2 pl-6">
+          <button class="bg-green-700 text-white mt-2 py-2 px-6 border border-transparent text-sm rounded-md font-semibold transition ease-out duration-300 hover:bg-white hover:text-green-700 hover:border-green-700">Trouver une activité</button>
+        </div>
+      </div>
     </section>
-
-    <!-- About Section -->
-    <section class="p-20 text-center">
-      <h2 class="text-3xl font-bold">About Us</h2>
-      <p class="mt-4 text-lg">
-        Discover the benefits of boxing and how we prepare you to become the best athlete you can be. Whether you're looking to compete or just get in shape, we're here for you.
-      </p>
-    </section>
-
-    <!-- Footer -->
-    <footer class="mt-auto bg-gray-800 text-white text-center p-4">
-      <p>Boxing Club © 2024</p>
-    </footer>
   </div>
 </template>
 
